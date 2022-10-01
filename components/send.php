ed03583c6c4e91e653b2948c4f860605546c0db9
Augusto <augustodiaz_1@hotmail.com>
2022-10-01 20:00:59 +1000

    PHP Mailer

---
+++ components/send.php
@@ -0,0 +1,53 @@
+<?php
+
+$name = $_POST['name'];
+$mail = $_POST['email'];
+$subject = $_POST['subject'];
+$body = $_POST['body'];
+
+require 'includes/PHPMailer.php';
+require 'includes/SMTP.php';
+require 'includes/Exception.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+if (isset($_POST["send"])) {
+
+    $email = new PHPMailer(true);
+
+    $email->isSMTP();
+    $email->Host = "smtp.gmail.com";
+    $email->SMTPAuth = "true";
+    $email->Username = "augustordiaz@gmail.com";
+    $email->Password = "hjndyrkibbqvlvay";
+    $email->SMTPSecure = "ssl";
+    $email->Port = 465;
+
+    $message =
+        "<h4 style='color: green; text-decoration: underline;'>Name: </h4>" . "<h2>$name</h2>" . "<br/>" .
+        "<h4 style='color: green; text-decoration: underline;'>Email: </h4>" . $mail . "<br/>" .
+        "<h4 style='color: green; text-decoration: underline;'>Subject: </h4>" . "<h3>$subject</h3>" . "<br/>" .
+        "<h4 style='color: green; text-decoration: underline;'>Message: </h4>" . "<p>$body</p>";
+
+    $email->setFrom("augustordiaz@gmail.com", 'Augusto Diaz');
+    $email->addAddress($_POST["email"]);
+    $email->isHTML(true);
+    $email->Subject = $_POST["subject"];
+    $email->Name = $_POST["name"];
+    $email->Body = $message;
+
+    if ($email->Send()) {
+        echo "
+        <script>alert('Email successfully sent!');
+        document.location.href = 'index.php'
+        </script>
+        ";
+    } else {
+        echo "
+        <script>alert('Error! Please, try again...');</script>
+        ";
+    };
+    $email->smtpClose();
+}
